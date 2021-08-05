692af152dc7d175940d4d610ee289e9573574846
malzak <malzovich@gmail.com>
2021-08-05 12:02:45 +0100

    auth components

--- src/pages/examples/Signup.js
+++ src/pages/examples/Signup.js
@@ -1,27 +1,86 @@
-
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faEnvelope, faUnlockAlt } from "@fortawesome/free-solid-svg-icons";
-import { faFacebookF, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { Col, Row, Form, Card, Button, FormCheck, Container, InputGroup } from '@themesberg/react-bootstrap';
-import { Link } from 'react-router-dom';
+import {
+  faAngleLeft,
+  faEnvelope,
+  faUnlockAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faGithub,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  Col,
+  Row,
+  Form,
+  Card,
+  Button,
+  FormCheck,
+  Container,
+  InputGroup,
+} from "@themesberg/react-bootstrap";
+import { Link } from "react-router-dom";
 
 import { Routes } from "../../routes";
 import BgImage from "../../assets/img/illustrations/signin.svg";
 
+export default (props) => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
 
-export default () => {
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+  const validate = () => {
+    if (password != confirmPassword) {
+      setPasswordError("Password doesnt match!");
+      return false;
+    } else {
+      setPasswordError(null);
+      return true;
+    }
+  };
+  const handleRegister = () => {
+    const isValid = validate();
+    if (isValid) {
+      axios({
+        method: "post",
+        url: `http://localhost:4000/register`,
+        data: {
+          email: email,
+          password: password,
+        },
+      })
+        .then((response) => {
+          props.history.push("/sign-in");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
   return (
     <main>
       <section className="d-flex align-items-center my-5 mt-lg-6 mb-lg-5">
         <Container>
-          <p className="text-center">
-            <Card.Link as={Link} to={Routes.DashboardOverview.path} className="text-gray-700">
-              <FontAwesomeIcon icon={faAngleLeft} className="me-2" /> Back to homepage
-            </Card.Link>
-          </p>
-          <Row className="justify-content-center form-bg-image" style={{ backgroundImage: `url(${BgImage})` }}>
-            <Col xs={12} className="d-flex align-items-center justify-content-center">
+          <Row
+            className="justify-content-center form-bg-image"
+            style={{ backgroundImage: `url(${BgImage})` }}
+          >
+            <Col
+              xs={12}
+              className="d-flex align-items-center justify-content-center"
+            >
               <div className="mb-4 mb-lg-0 bg-white shadow-soft border rounded border-light p-4 p-lg-5 w-100 fmxw-500">
                 <div className="text-center text-md-center mb-4 mt-md-0">
                   <h3 className="mb-0">Create an account</h3>
@@ -33,7 +92,13 @@ export default () => {
                       <InputGroup.Text>
                         <FontAwesomeIcon icon={faEnvelope} />
                       </InputGroup.Text>
-                      <Form.Control autoFocus required type="email" placeholder="example@company.com" />
+                      <Form.Control
+                        autoFocus
+                        required
+                        type="email"
+                        placeholder="example@company.com"
+                        onChange={handleEmailChange}
+                      />
                     </InputGroup>
                   </Form.Group>
                   <Form.Group id="password" className="mb-4">
@@ -42,7 +107,12 @@ export default () => {
                       <InputGroup.Text>
                         <FontAwesomeIcon icon={faUnlockAlt} />
                       </InputGroup.Text>
-                      <Form.Control required type="password" placeholder="Password" />
+                      <Form.Control
+                        required
+                        type="password"
+                        placeholder="Password"
+                        onChange={handlePasswordChange}
+                      />
                     </InputGroup>
                   </Form.Group>
                   <Form.Group id="confirmPassword" className="mb-4">
@@ -51,7 +121,15 @@ export default () => {
                       <InputGroup.Text>
                         <FontAwesomeIcon icon={faUnlockAlt} />
                       </InputGroup.Text>
-                      <Form.Control required type="password" placeholder="Confirm Password" />
+                      <Form.Control
+                        required
+                        type="password"
+                        placeholder="Confirm Password"
+                        onChange={handleConfirmPasswordChange}
+                      />
+                      <div className="text-start w-100 invalid-feedback d-block">
+                        {passwordError}
+                      </div>
                     </InputGroup>
                   </Form.Group>
                   <FormCheck type="checkbox" className="d-flex mb-4">
@@ -61,7 +139,12 @@ export default () => {
                     </FormCheck.Label>
                   </FormCheck>
 
-                  <Button variant="primary" type="submit" className="w-100">
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="w-100"
+                    onClick={handleRegister}
+                  >
                     Sign up
                   </Button>
                 </Form>
@@ -70,20 +153,33 @@ export default () => {
                   <span className="fw-normal">or</span>
                 </div>
                 <div className="d-flex justify-content-center my-4">
-                  <Button variant="outline-light" className="btn-icon-only btn-pill text-facebook me-2">
+                  <Button
+                    variant="outline-light"
+                    className="btn-icon-only btn-pill text-facebook me-2"
+                  >
                     <FontAwesomeIcon icon={faFacebookF} />
                   </Button>
-                  <Button variant="outline-light" className="btn-icon-only btn-pill text-twitter me-2">
+                  <Button
+                    variant="outline-light"
+                    className="btn-icon-only btn-pill text-twitter me-2"
+                  >
                     <FontAwesomeIcon icon={faTwitter} />
                   </Button>
-                  <Button variant="outline-light" className="btn-icon-only btn-pil text-dark">
+                  <Button
+                    variant="outline-light"
+                    className="btn-icon-only btn-pil text-dark"
+                  >
                     <FontAwesomeIcon icon={faGithub} />
                   </Button>
                 </div>
                 <div className="d-flex justify-content-center align-items-center mt-4">
                   <span className="fw-normal">
                     Already have an account?
-                    <Card.Link as={Link} to={Routes.Signin.path} className="fw-bold">
+                    <Card.Link
+                      as={Link}
+                      to={Routes.Signin.path}
+                      className="fw-bold"
+                    >
                       {` Login here `}
                     </Card.Link>
                   </span>
