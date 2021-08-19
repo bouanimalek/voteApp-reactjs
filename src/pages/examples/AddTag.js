3947903f62f463f37cc128a6b3e8419898502489
malzak <malzovich@gmail.com>
2021-08-19 11:38:23 +0100

    adding form validation in AddTag

--- src/pages/examples/AddTag.js
+++ src/pages/examples/AddTag.js
@@ -16,64 +16,106 @@ import Profile3 from "../../assets/img/team/profile-picture-3.jpg";
 import TagService from "../../services/tag.services";
 import { toast } from "react-toastify";
 
-export default () => {
+export default (props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [nameRequired, setNameRequired] = useState("");
+  const [descriptionRequired, setDescriptionRequired] = useState("");
+
+  const validate = () => {
+    let isValidForm = false;
+    if (!name) {
+      setNameRequired("Name is required!");
+    } else {
+      setNameRequired(null);
+    }
+    if (!description) {
+      setDescriptionRequired("Description is required!");
+    } else {
+      setDescriptionRequired(null);
+    }
+    if (name && description) {
+      isValidForm = true;
+    }
+    return isValidForm;
+  };
 
   const handleSave = () => {
-    TagService.createTag({ name, description })
-      .then((response) => {
-        console.log(response.data);
-        toast.success("Tag created successfully!");
-        setName("");
-        setDescription("");
-      })
-      .catch((error) => {
-        console.log(error);
-        toast.error(error.response.data.message);
-        //console.log(name, description, price, startDate.toString(), address);
-      });
+    const isValid = validate();
+    if (isValid) {
+      TagService.createTag({ name, description })
+        .then((response) => {
+          console.log(response.data);
+          toast.success("Tag created successfully!");
+          setName("");
+          setDescription("");
+          props.history.push("/tags");
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error(error.response.data.message);
+          //console.log(name, description, price, startDate.toString(), address);
+        });
+    }
   };
 
   return (
     <>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4"></div>
       <Row>
-        <Col xs={12} xl={8}>
+        <Col xs={12} xl={12}>
           <Card border="light" className="bg-white shadow-sm mb-4 ">
             <Card.Body>
-              <h5 className="mb-4">Add Tag</h5>
+              <h5 className="mb-4">Add New Tag</h5>
               <Form>
                 <Row>
                   <Col md={6} className="mb-3">
                     <Form.Group id="name">
                       <Form.Label>Tag Name</Form.Label>
-                      <Form.Control
-                        required
-                        type="text"
-                        value={name}
-                        placeholder="Enter your tag name"
-                        onChange={(e) => setName(e.target.value)}
-                      />
+                      <InputGroup>
+                        <Form.Control
+                          required
+                          type="text"
+                          value={name}
+                          placeholder="Enter your tag name"
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                        <div className="text-start w-100 invalid-feedback d-block">
+                          {nameRequired}
+                        </div>
+                      </InputGroup>
                     </Form.Group>
                   </Col>
                   <Col md={6} className="mb-3">
                     <Form.Group id="description">
                       <Form.Label>Tag Description</Form.Label>
-                      <Form.Control
-                        required
-                        as="textarea"
-                        value={description}
-                        placeholder="Enter tag description"
-                        onChange={(e) => setDescription(e.target.value)}
-                      />
+                      <InputGroup>
+                        <Form.Control
+                          required
+                          as="textarea"
+                          value={description}
+                          placeholder="Enter tag description"
+                          onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <div className="text-start w-100 invalid-feedback d-block">
+                          {descriptionRequired}
+                        </div>
+                      </InputGroup>
                     </Form.Group>
                   </Col>
                 </Row>
 
                 <div className="mt-3">
                   <Button variant="primary" type="button" onClick={handleSave}>
-                    Save All
+                    <i className="fa fa-save"></i> Save
+                  </Button>
+                  <Button
+                    variant="primary ms-1"
+                    type="button"
+                    onClick={() => props.history.push("/tags")}
+                  >
+                    {" "}
+                    <i className="fa fa-undo"></i> Cancel
                   </Button>
                 </div>
               </Form>
